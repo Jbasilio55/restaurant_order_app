acf5383662921ab7bcdcf6641bd18cc78caa0376
Jbasilio55 <j.l.basilio55@gmail.com>
2024-10-06 21:56:48 -0400

    completed most of order section

--- script.js
+++ script.js
@@ -43,25 +43,37 @@ menuCards.addEventListener("click", (e) => {
     }
   }
 
-  console.log(cart);
-
   if (cart.length > 0) {
     order.style.display = "block";
     container.style.overflow = "scroll";
   }
 
-  const orderItem = cart.map((item) => {
-    return `
-      <div class="order-items">
-      <div class="order-items-container">
-        <h3>${item.name} x( ${item.quantity} )</h3>
-        <a href="#">remove</a>
-      </div>
-        <p>$${item.price * item.quantity}</p>
-      </div>`;
-  });
+  updateOrder();
+});
 
-  itemContainer.innerHTML = orderItem.join("");
+total.innerHTML += `<div class="total-price">
+                        <h2>Total Price:</h2>
+                        <h3>Price$$$</h3>
+                    </div>`;
+
+const remove = document.querySelector(".remove");
+
+order.addEventListener("click", (e) => {
+  let id = Number(e.target.dataset.item);
+  console.log(cart);
+
+  for (let item of cart) {
+    if (item.id === id) {
+      removeItem(item);
+    }
+  }
+
+  if (cart.length < 1) {
+    order.style.display = "none";
+    container.style.overflow = "hidden";
+  }
+
+  updateOrder();
 });
 
 function addItem(item) {
@@ -71,4 +83,29 @@ function addItem(item) {
   } else {
     item.quantity++;
   }
+}
+
+function removeItem(item) {
+  let index = cart.findIndex((i) => i === item);
+
+  if (item.quantity < 2) {
+    cart.splice(index, 1);
+  } else {
+    item.quantity--;
+  }
+}
+
+function updateOrder() {
+  let orderItem = cart.map((item) => {
+    return `
+          <div class="order-items">
+          <div class="order-items-container">
+            <h3>${item.name} x( ${item.quantity} )</h3>
+            <a href="#" class="remove" data-item="${item.id}">remove</a>
+          </div>
+            <p>$${item.price * item.quantity}</p>
+          </div>`;
+  });
+
+  itemContainer.innerHTML = orderItem.join("");
 }
